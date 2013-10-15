d12ad78cee3cd1eee5a84b4ffea0f21b0f8b138f
Amir Taaki <genjix@riseup.net>
2013-10-15 16:18:45 +0000

    added missing file.

--- src/importdb.cpp
+++ src/importdb.cpp
@@ -1,16 +1,11 @@
 #include "transaction_database.hpp"
 #include <leveldb/db.h>
 #include <bitcoin/bitcoin.hpp>
-#include <bitcoin/utility/timed_section.hpp>
 
 using namespace bc;
 
 int main()
 {
-    constexpr size_t total_number = 100000;
-    log_debug() << "Storing " << total_number << " txs";
-    std::vector<hash_digest> tx_hashes;
-    tx_hashes.reserve(total_number);
     // create and alloc file
     transaction_database txdb("../tx.db");
 
@@ -19,43 +14,27 @@ int main()
     leveldb::Status status = leveldb::DB::Open(options, "blockchain/tx", &db);
     assert(status.ok());
 
-    timed_section* t = new timed_section("benchmark", "storing");
-    size_t i = 0;
     leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
     for (it->SeekToFirst(); it->Valid(); it->Next())
     {
-        ++i;
-        if (i > total_number)
-            break;
         const uint8_t* hash_data =
             reinterpret_cast<const uint8_t*>(it->key().data());
         assert(it->key().size() == 32);
         hash_digest tx_hash;
         std::copy(hash_data, hash_data + 32, tx_hash.begin());
-        tx_hashes.push_back(tx_hash);
         const uint8_t* value =
             reinterpret_cast<const uint8_t*>(it->value().data()) + 8;
         transaction_type tx;
         satoshi_load(value, value + it->value().size(), tx);
         BITCOIN_ASSERT(hash_transaction(tx) == tx_hash);
+        // Display every 1000th tx.
+        if (rand() % 1000 == 0)
+            log_debug() << "Importing: " << tx_hash;
         txdb.store(tx);
     }
-    delete t;
     assert(it->status().ok());
     delete it;
     delete db;
-
-    log_debug() << "Reading " << total_number << " txs";
-
-    t = new timed_section("benchmark", "storing");
-    for (const hash_digest& tx_hash: tx_hashes)
-    {
-        transaction_type tx;
-        bool success = txdb.get(tx_hash, tx);
-        BITCOIN_ASSERT(success);
-        BITCOIN_ASSERT(hash_transaction(tx) == tx_hash);
-    }
-    delete t;
 
     return 0;
 }
